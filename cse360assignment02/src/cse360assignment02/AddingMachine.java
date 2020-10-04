2a90ad507389f21725ee29ccd624cc2a25fe57a9
jneuenda <jneuenda@asu.edu>
2020-10-04 00:35:03 -0500

    Added Comments

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -3,24 +3,49 @@ package cse360assignment02;
 public class AddingMachine {
 	  private int total;
 	  
+	  /**
+	  *Addingmachine calls the add and subtract
+	  * methods to do arithmetic 
+	  **/
 	  public AddingMachine () {
 	    total = 0;  // not needed - included for clarity 
 	  }
 	  
+	  /**
+	  *getTotal returns the private total variable 
+	  *@return total int
+	  **/
 	  public int getTotal () {
 	    return 0;
 	  }
 	  
+	  /**
+		  *add will add given numbers to the private total variable 
+		  *@param value int 
+		  *@return void 
+		  **/
 	  public void add (int value) {
 	  }
-
+	  
+	  /**
+		  *subtract will subtract a given number from the total
+		  *@param value int 
+		  *@return void 
+	  **/
 	  public void subtract (int value) {
 	  }
-
+	  
+	  /**
+	  *toString will return a string of all transactions
+	  *@return returnString String
+	  **/
 	  public String toString () {
 	    return "";
 	  }
-
+	  /**
+		  *clear clears the total variable 
+		  *@return void
+		  **/
 	  public void clear() {
 	  }
 	}
